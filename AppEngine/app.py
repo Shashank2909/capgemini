e9e136c5a10a72e6a1c967592f7a82a21bf214ce
Shashank2909 <sajgaonkar007@gmail.com>
2021-11-24 16:43:22 +0530

    App Engine

---
+++ AppEngine/app.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify,request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['Post'])
+def bmi():
+    data = request.get_json()
+    height = data['height']
+    weight = data['weight']
+    b = (weight/(height**2))* 10**4
+    return jsonify({"body mass index":b})
+
+if __name__ =="__main__":
+    app.run(debug=True,port=5000)   
+
